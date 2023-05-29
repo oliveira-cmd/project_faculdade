29eb9e4a17779b5de7064a69e3a9ea9ee745f98a
Gabriel Oliveira <oliveira.gabrielwilliam@gmail.com>
2023-05-29 20:40:50 -0300

    subindo projeto no github

--- js/index.js
+++ js/index.js
@@ -1,21 +1,65 @@
-function generateCurriculum(){
+function generateCurriculum() {
+  const username = document.getElementById('name').value;
+  const age = document.getElementById('age').value;
+  const email = document.getElementById('email').value;
+  const whatsapp = document.getElementById('whatsapp').value;
+  const github = document.getElementById('github').value;
+  const about_me = document.getElementById('bio').value;
+  const url = `https://api.github.com/users/${github}/repos`;
+  const image_user = `https://github.com/${github}.png`;
+  const curriculumDiv = document.querySelector('body');
 
-    var link = "https://github.com/oliveira-cmd?tab=repositories";
-    var proxy = "https://cors-anywhere.herokuapp.com/";
-
-    scrapper = function(link){
-        let x = new XMLHttpRequest();
-        x.onreadystatechange = function(){
-            if(this.readyState==4 && this.status == 200){
-                let response = this.responseText;
-                document.body.innerHTML = response;
-            }    
-        }
-
-        x.open("GET",link);
-        x.send();
-    }
-
-    scrapper(proxy+link);
-
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if(data.message != 'Not Found'){
+        const repos = data.map(repo => repo.name);
+        const privacy = data.map(repo => repo.private);
+  
+  
+        curriculumDiv.innerHTML = `
+          <div class="container mx-auto p-4">
+            <h1 class="text-3xl font-bold mb-4">Currículo de  ${username} </h1>
+        
+            <div class="bg-white shadow-md p-6 rounded-lg flex justify-between  ">
+              <div class="personal_informations">
+                <h2 class="text-2xl font-bold mb-2">Informações Pessoais</h2>
+                <p><strong>Nome:</strong> ${username}</p>
+                <p><strong>Idade:</strong> ${age}</p>
+                <p><strong>Email:</strong> ${email} </p>
+                <p><strong>Telefone:</strong> ${whatsapp}</p>
+                <p><strong>Sobre mim:</strong> ${about_me}</p>
+              </div>
+              <div class="bg-white-300">
+                <img class="rounded-full" src="${image_user}">
+              </div>
+            </div>
+        
+            <div class="bg-white shadow-md p-6 mt-4 rounded-lg">
+              <h2 class="text-2xl font-bold mb-2">Projetos Realizados</h2>
+              <div class="mb-4">
+                ${repos.map(
+                  repo => ` 
+                  <a href="https://github.com/${github}/${repo}">
+                    <li>${repo} -  ${(!privacy ? 'Repositório Privado' : 'Repositório Público')}</li>
+                  </a>
+                  <a>
+                  </a>
+                  `
+                ).join('')}
+              </div>
+            </div>
+          </div>
+          <style>
+            img.rounded-full {
+              display:flex;
+              max-width:150px!important;
+            }
+          </style>
+        `;
+      } else {
+        curriculumDiv.innerHTML = `O usuario ${github} não existe no github, atualize a página e tente novamente`;
+      }
+    })
+    .catch(error => console.error(error));
 }
